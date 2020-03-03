391b9732effb03036d3d2b8ec817ecbccc8d6ddb
Rohan Batra <rohanbatra24@gmail.com>
2020-03-03 21:37:58 +0000

    created assertEqual function

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual([ 1, 2, 3 ], [ 1, 2, 3 ]);
+assertEqual([ 1, 2, 3 ], [ 1, 2, 7 ]);
+assertEqual([ 1, 2, 3 ], [ 1, 2, 3, 4 ]);
