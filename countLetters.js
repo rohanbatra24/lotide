0b78e007ae1e40764be2b0f5894fa1b128dbfb0d
Rohan Batra <rohanbatra24@gmail.com>
2020-03-04 20:36:34 +0000

    count letters function

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (string) => {
+  let noSpaces = string.replace(/ /g, '');
+
+  console.log(noSpaces);
+
+  const results = {};
+
+  for (let letter of noSpaces) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else results[letter] = 1;
+  }
+
+  return results;
+};
+
+const result1 = countLetters('Lighthouse in the house');
+
+assertEqual(result1['h'], 4);
+assertEqual(result1['L'], 1);
+assertEqual(result1['s'], 2);
