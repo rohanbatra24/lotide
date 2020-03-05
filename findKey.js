259797bd4876e521771bac96b1bfda70a2a22561
Rohan Batra <rohanbatra24@gmail.com>
2020-03-05 23:12:26 +0000

    find key function

---
+++ findKey.js
@@ -0,0 +1,19 @@
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const obj = {
+  'Blue Hill': { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 }
+};
+
+console.log(findKey(obj, (x) => x.stars === 9)); // => "noma"
