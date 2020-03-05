9b305f57699181ce182d56e0b51bdb4192ee2afb
Rohan Batra <rohanbatra24@gmail.com>
2020-03-05 00:37:08 +0000

    assert object equal function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,34 @@
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+
+  for (let key in object1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) return false;
+  }
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  // Implement me!
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+assertObjectsEqual({ c: '1', d: [ '2', 3, 5 ] }, { d: [ '2', 3 ], c: '1' });
